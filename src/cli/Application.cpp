7752f6c34fc39c817c7b0d4746c7aef830db74d7
Yuliya <xenerizes@users.noreply.github.com>
2018-05-27 20:54:09 +0300

    Get rid of Loader

--- src/cli/Application.cpp
+++ src/cli/Application.cpp
@@ -1,14 +1,11 @@
 #include "cli/Application.hh"
 #include "cli/Settings.hh"
-#include "client/Loader.hh"
-#include "writer/Writer.hh"
+#include "lib/writer/Writer.hh"
 
 
 void Application::run(const Settings& settings)
 {
-    Loader loader(settings.client);
     Writer writer(settings.filename);
-    auto&& data = loader.download(settings.url);
+    auto&& data = settings.client.download(settings.url);
     writer.write(data);
 }
-
