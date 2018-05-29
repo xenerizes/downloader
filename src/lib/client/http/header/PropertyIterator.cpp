5d63d1148b60f86e4f513fe436ff45d94db8841e
Yuliya <xenerizes@users.noreply.github.com>
2018-05-30 00:34:09 +0300

    Fix iterator errors

--- src/lib/client/http/header/PropertyIterator.cpp
+++ src/lib/client/http/header/PropertyIterator.cpp
@@ -11,7 +11,6 @@ constexpr const char* HTTP_1_1_PROPERTY = "HTTP/1.1 ";
 constexpr const char* CONTENT_LENGTH_PROPERTY = "Content-Length: ";
 constexpr const char* TRANSFER_ENCODING_PROPERTY = "Transfer-Encoding: ";
 
-
 PropertyIterator::PropertyIterator(const Buffer& buf) : buf_(buf), pos_(0) { }
 
 HeaderPropertyPtr PropertyIterator::next()
@@ -22,7 +21,7 @@ HeaderPropertyPtr PropertyIterator::next()
     do {
         line = getline();
         next_prop = make_property(line);
-    } while (next_prop->type == PropertyType::EMPTY && line != Line(0, 0));
+    } while (next_prop->type == PropertyType::EMPTY && line.first != line.second);
 
     return next_prop;
 }
@@ -35,11 +34,12 @@ Line PropertyIterator::getline()
     for (size_t i = buf_.find(CARRIAGE_RET, pos_); start < BUFFER_SIZE; ) {
         if (buf_.data()[i + 1] == NEWLINE) {
             pos_ = i + 2;
-            return Line(start, i - 1);
+            if (start == i) break;
+            return Line(start, i);
         }
     }
 
-    return Line(0, 0);
+    return Line(pos_, pos_);
 }
 
 HeaderPropertyPtr PropertyIterator::make_property(const Line& line)
@@ -48,17 +48,21 @@ HeaderPropertyPtr PropertyIterator::make_property(const Line& line)
         if (!starts_with(line, HTTP_1_1_PROPERTY)) {
             throw std::runtime_error("Unsupported response protocol");
         }
-        auto start = line.first + strlen(HTTP_1_1_PROPERTY);
+        auto start = line.first + strlen(HTTP_1_1_PROPERTY) - 1;
         return std::make_unique<CodeProperty>(Line(start, line.second), buf_);
     }
 
-    if (starts_with(line, CONTENT_LENGTH_PROPERTY)) {
-        auto start = line.first + strlen(CONTENT_LENGTH_PROPERTY);
-        return std::make_unique<ContentLengthProperty>(Line(start, line.second), buf_);
-    }
+    if (line.first != line.second) {
 
-    if (starts_with(line, TRANSFER_ENCODING_PROPERTY)) {
-        throw std::runtime_error("Transfer encoding is not supported");
+        if (starts_with(line, CONTENT_LENGTH_PROPERTY)) {
+            auto start = line.first + strlen(CONTENT_LENGTH_PROPERTY);
+            return std::make_unique<ContentLengthProperty>(
+                Line(start, line.second), buf_);
+        }
+
+        if (starts_with(line, TRANSFER_ENCODING_PROPERTY)) {
+            throw std::runtime_error("Transfer encoding is not supported");
+        }
     }
 
     return std::make_unique<EmptyProperty>(buf_);
@@ -68,7 +72,7 @@ bool PropertyIterator::starts_with(const Line& l, const char* prefix)
 {
     auto len = strlen(prefix);
     for (size_t i = l.first; i < l.first + len; i++) {
-        if (i >= l.second || buf_.data()[i] != prefix[i]) {
+        if (i >= l.second || buf_.data()[i] != prefix[i - l.first]) {
             return false;
         }
     }
