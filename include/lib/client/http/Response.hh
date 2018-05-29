15e18d12b8cc304fe1900b1dd2fdabfa221f793d
Yuliya <xenerizes@users.noreply.github.com>
2018-05-30 02:13:34 +0300

    Buffer sizes in Response

--- include/lib/client/http/Response.hh
+++ include/lib/client/http/Response.hh
@@ -7,4 +7,8 @@ struct Response {
     std::string code;
     size_t content_length;
     size_t header_length;
+
+    long first_buffer(size_t buf_size);
+    long full_buffers(size_t buf_size);
+    long last_buffer_size(size_t buf_size);
 };
---
+++ src/lib/client/http/Response.cpp
@@ -0,0 +1,20 @@
+#include "lib/client/http/Response.hh"
+
+#include <cmath>
+
+long Response::first_buffer(size_t buf_size)
+{
+    return buf_size - header_length;
+}
+
+long Response::full_buffers(size_t buf_size)
+{
+    auto first_buffer = first_buffer(buf_size);
+    return std::trunc((content_length - first_buffer) / buf_size);
+}
+
+long Response::last_buffer_size(size_t buf_size)
+{
+    auto buffers = full_buffers(buf_size);
+    return buffers * buf_size - content_length - first_buffer(buf_size);
+}
