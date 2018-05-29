145b321d60bd04338a011dd85869a1bf516c9e87
Yuliya <xenerizes@users.noreply.github.com>
2018-05-29 21:47:17 +0300

    Merged append calls

--- src/lib/client/http/header/HttpHeader.cpp
+++ src/lib/client/http/header/HttpHeader.cpp
@@ -4,12 +4,8 @@
 Buffer HttpHeader::build_request(const Request& req)
 {
     Buffer buf;
-    buf.append("GET ");
-    buf.append(req.path);
-    buf.append(" HTTP/1.1\r\n");
-    buf.append("Host: ");
-    buf.append(req.hostname);
-    buf.append("\r\n");
+    buf.append("GET " + req.path + " HTTP/1.1\r\n");
+    buf.append("Host: " + req.hostname + "\r\n");
     buf.append("Accept: */*\r\n");
     buf.append("Connection: close\r\n\r\n");
 
